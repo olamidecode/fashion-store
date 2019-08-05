fa7e442729a4a169cd3e6237efaba413c626bfd4
olamidecode <olaakinde16@gmail.com>
2019-08-05 15:01:57 +0200

    added media querys and styling

---
+++ js/shop.js
@@ -0,0 +1,10 @@
+//   nav bar js
+function Function2() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  
